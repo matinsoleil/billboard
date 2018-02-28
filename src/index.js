0116c485bce2bfcd59c1b5bd73b1da5b9859e4b5
adrian <aromero@orionworkflow.net>
2018-02-28 17:10:42 -0600

    up

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import { render } from 'react-dom';
 import App from './components/App';
 import Message from './components/Message';
 render(<App name='World' />, document.getElementById('root'));
+render(<Message name="Me" />, document.getElementById('message'));
