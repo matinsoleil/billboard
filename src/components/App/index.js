29f98c5f407c80c0777e2dba99d33b83a355698a
adrian <aromero@orionworkflow.net>
2018-02-28 17:00:50 -0600

    up

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,8 +10,8 @@ function App({ name }) {
   );
 };
 
-App.propTypes ={
-               name: PropTypes.string,
-               };
+App.propTypes = {
+  name: PropTypes.string,
+};
 
 export default App;
