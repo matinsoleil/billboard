c7db043aed1e10bb2189b620ccbf7d539c69436f
adrian <aromero@orionworkflow.net>
2018-02-28 17:28:33 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import Message from './components/Message';
-render(<App name='World' />, document.getElementById('root'));
-render(<Message name="Me" />, document.getElementById('root'));
+
+
+var Component = React.createClass({
+    render: function(){
+    return(
+    <div>
+        <App />
+        <Message />
+    </div>
+    )
+    }
+});
+
+render(<Component />, document.getElementById('root'));
+
+//render(<App name='World' />, document.getElementById('root'));
+//render(<Message name="Me" />, document.getElementById('root'));
