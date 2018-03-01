b5bae2e25211a7e3d91769b9e6acf442b70b72d0
adrian <aromero@orionworkflow.net>
2018-03-01 12:06:29 -0600

    up

--- src/components/Copy/index.js
+++ src/components/Copy/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import 'src/assets/stylesheets/base.scss';
 
-class Copy extends React.Component{
-render(){
-const name = this.props.name
-return (<div>{name}</div>);
+const Copy = React.createClass({
+propTypes:{
+
+},
+getDefaultProps(){
+
+},
+render()
+{
+ return(<div>AUHHHCH</div>);
 }
-}
-
-Copy.propTypes = {
-  name: PropTypes.string,
-};
+});
 
 export default Copy;
