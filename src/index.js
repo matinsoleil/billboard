96eb847ec1d5ac3f6ba3e457b0a9ef06730cd7d1
adrian <aromero@orionworkflow.net>
2018-02-28 18:40:25 -0600

    task

--- src/index.js
+++ src/index.js
@@ -5,5 +5,17 @@ import Message from './components/Message';
 
 render(<App name='World' />, document.getElementById('root'));
 
+var All = React.createClass({
+       render: function(){return(
+                    <div>
+                     <App name='World' />
+                    <Message name='Help' />
+                     </div>
+               );}
+});
+
+render(<All />,document.getElementById('root'));
+
+
 //render(<App name='World' />, document.getElementById('root'));
 //render(<Message name="Me" />, document.getElementById('root'));
