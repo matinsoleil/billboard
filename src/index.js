8e5437dec09c192de9c1d7f3c96d26b9a49f58cf
adrian <aromero@orionworkflow.net>
2018-02-28 20:08:21 -0600

    up

--- src/index.js
+++ src/index.js
@@ -3,27 +3,5 @@ import { render } from 'react-dom';
 import App from './components/App';
 import Message from './components/Message';
 
-var ItemList = React.createClass({
-    render: function() {
-        console.log(this.props);
-
-        var items = this.props.data["items"].map(function(itemData) {
-            var component = Components[itemData['itemClass']];
-            return React.createElement(component, {
-                data: itemData,
-                key: itemData['id']
-            });
-        });
-        console.log(items);
-        return (
-            <div className="list">
-                <div>And I am an ItemList</div>
-                <div>{items}</div>
-            </div>
-        );
-    }
-});
-
-
 render(<div><App name="york" /><Message name='You' /></div>, document.getElementById('root'));
 
