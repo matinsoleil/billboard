5fa9a1f65034b1ee16c32f22000ac75abfb71ac8
adrian <aromero@orionworkflow.net>
2018-02-28 18:22:50 -0600

    up

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import { render } from 'react-dom';
 import App from './components/App';
 import Message from './components/Message';
 
-
-var Component = React.createClass({
-    render: function(){
-    return(
-    <div>
-        <App />
-        <Message />
-    </div>
-    )
-    }
-});
-
-render(<Component />, document.getElementById('root'));
+render(<App /><Message />, document.getElementById('root'));
 
 //render(<App name='World' />, document.getElementById('root'));
 //render(<Message name="Me" />, document.getElementById('root'));
