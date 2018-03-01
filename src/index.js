ed4d90becb493e59ad0ba0282d0054c5d4c5f2dd
adrian <aromero@orionworkflow.net>
2018-02-28 20:02:49 -0600

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,5 @@ import App from './components/App';
 import Message from './components/Message';
 
 
-var component = Components[itemData['itemClass']]);
-return React.createElement(component, {
-    data: itemData,
-    key: itemData['id']
-});
-
 render(<div><App name="york" /><Message name='You' /></div>, document.getElementById('root'));
 
