ee95a69731905c05a794550dd9bec37b528ef251
adrian <aromero@orionworkflow.net>
2018-03-01 12:21:38 -0600

    u

--- src/components/Copy/index.js
+++ src/components/Copy/index.js
@@ -1,19 +1,1 @@
-import PropTypes from 'prop-types';
 
-class Copy extends React.Component {
-  render() {
-    // This must be exactly one element or it will warn.
-    const children = this.props.children;
-    return (
-      <div>
-        {children}
-      </div>
-    );
-  }
-}
-
-Copy.propTypes = {
-  children: PropTypes.element.isRequired
-};
-
-export default Copy;
