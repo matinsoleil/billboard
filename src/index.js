21c5310581d70372d7fc9597ccd097a52148dc0e
adrian <aromero@orionworkflow.net>
2018-02-28 19:32:48 -0600

    u

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ import Message from './components/Message';
 //render(){return [<App name="york" >,<Message name="help">]}
 
 
-render(<div><App name="york" /><Message name='You' /></div>, document.getElementById('footer'));
+render(<div><App name="york" /><Message name='You' /></div>, document.getElementById('root'));
 
