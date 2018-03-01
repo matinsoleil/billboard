387acaa8e6123b590a7b0e691dd81435286b04f5
adrian <aromero@orionworkflow.net>
2018-02-28 20:11:31 -0600

    up

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import { render } from 'react-dom';
 import App from './components/App';
 import Message from './components/Message';
 
+class Hello extends React.Component {
+  render() {
+    return <div>Hello {this.props.toWhat}</div>;
+  }
+}
+
 render(<div><App name="york" /><Message name='You' /></div>, document.getElementById('root'));
 
