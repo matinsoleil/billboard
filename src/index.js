a1b76df37ca436df42a40a7ea4781e6a7ef732d0
adrian <aromero@orionworkflow.net>
2018-03-01 15:24:31 -0600

    help

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-
-render(<App name='World' />, document.getElementById('root'));
+class Hello extends React.Component {render() {return <div>Hello {this.props.toWhat}</div>;}}
+render(<div><App name='World' /><Hello toWhat='help' /></div>, document.getElementById('root'));
