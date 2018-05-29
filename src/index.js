0604419d938c4ef773b4110cbdd2d40fdecc96e5
adrian <aromero@orionworkflow.net>
2018-05-29 13:11:22 -0500

    up

--- src/index.js
+++ src/index.js
@@ -9,33 +9,16 @@ var http = require('http');
 
 var simple ="texto";
 
-function getXML(callback) {
-
-    return http.get({
-        host: 'orion-component.herokuapp.com',
-        path: '/src/layout.xml'
-    }, function(response) {
-        // Continuously update stream with data
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('end', function() {
-
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            callback({
-                src: body,
-            });
-        });
-    });
-
-},
-
-
 
 import Service from './components/Service';
 var DOMParser = require('xmldom').DOMParser;
-class Hello extends React.Component {render() {
-return <div>Hello {this.props.toWhat}</div>;}}
+
+class Hello extends React.Component {
+
+render() {
+return <div>Hello {this.props.toWhat}</div>;
+}
+
+}
+
 render(<div><App name='World' /><span>simple</span><Hello toWhat='help' /><Service name='wilfrido' /></div>, document.getElementById('root'));
