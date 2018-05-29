2c05304247599011180d0f57168ff85f361d06c3
adrian <aromero@orionworkflow.net>
2018-05-29 11:01:14 -0500

    layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { render } from 'react-dom';
 import App from './components/App';
+var DOMParser = require('xmldom').DOMParser;
 import Service from './components/Service';
 class Hello extends React.Component {render() {return <div>Hello {this.props.toWhat}</div>;}}
 render(<div><App name='World' /><Hello toWhat='help' /><Service name='wilfrido' /></div>, document.getElementById('root'));
