aec7c21b7d933afb2179b7d9e6a6e9b7182c93d7
adrian <aromero@orionworkflow.net>
2018-05-29 12:45:49 -0500

    up

--- src/index.js
+++ src/index.js
@@ -2,25 +2,36 @@ import React from 'react';
 import axios from 'axios';
 import { render } from 'react-dom';
 import App from './components/App';
-var http = require('http');
-var options = {
-    host: 'orion-component.herokuapp.com',
-    path: '/src/layout.xml'
-}
-var request = http.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        console.log(data);
 
+
+
+var http = require('http');
+
+var simple ="texto";
+
+function getXML(callback) {
+
+    return http.get({
+        host: 'orion-component.herokuapp.com',
+        path: '/src/layout.xml'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback({
+                src: body,
+            });
+        });
     });
-});
-request.on('error', function (e) {
-    console.log(e.message);
-});
-request.end();
+
+},
+
 
 
 
@@ -28,4 +39,4 @@ request.end();
 import Service from './components/Service';
 var DOMParser = require('xmldom').DOMParser;
 class Hello extends React.Component {render() {return <div>Hello {this.props.toWhat}</div>;}}
-render(<div><App name='World' /><span>{request}</span><Hello toWhat='help' /><Service name='wilfrido' /></div>, document.getElementById('root'));
+render(<div><App name='World' /><span>{simple}</span><Hello toWhat='help' /><Service name='wilfrido' /></div>, document.getElementById('root'));
