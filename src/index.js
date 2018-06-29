08ceef22addc095df0b41562847db9d657347c6d
adrian <aromero@orionworkflow.net>
2018-06-29 14:21:12 -0500

    u

--- src/index.js
+++ src/index.js
@@ -25,35 +25,43 @@ getXML(){
 
 var http = require('http');
 
-var options = {
-  host:  'orion-component.herokuapp.com',
-  path: '/layout'
-};
 
-var body='';
+var url = 'orion-component.herokuapp.com',
 
-body=http.get(options, function(res) {
-  res.on('data', function(chunk) {
-    body += chunk;
-  });
-  res.on('end', function() {
-    this.callback(body);
-  });
 
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
+data=this.getContent(url);
 
-console.log(this.data);
+console.log(data);
 
 return "help";
 }
 
-callback(body){
 
-this.data = body;
+getContent(url){
+
+return new Promise((resolve, reject) => {
+    // select http or https module, depending on reqested url
+    const lib = url.startsWith('https') ? require('https') : require('http');
+    const request = lib.get(url, (response) => {
+      // handle http errors
+      if (response.statusCode < 200 || response.statusCode > 299) {
+         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+       }
+      // temporary data holder
+      const body = [];
+      // on every content chunk, push it to the data array
+      response.on('data', (chunk) => body.push(chunk));
+      // we are done, resolve promise with those joined chunks
+      response.on('end', () => resolve(body.join('')));
+    });
+    // handle connection errors of the request
+    request.on('error', (err) => reject(err))
+    })
+
 
 }
+
+
 
 }
 
