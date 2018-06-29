c681062e91b8ae8dea7fe3595267ec73a9f19ab0
adrian <aromero@orionworkflow.net>
2018-06-29 12:54:47 -0500

    u

--- src/index.js
+++ src/index.js
@@ -31,11 +31,23 @@ var options = {
 };
 
 
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+
 
 return "help";
-
-
-        }
+}
 
 }
 
