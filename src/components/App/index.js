26eb688174885db942254bd5a8d2a30ab7647254
adrian <aromero@orionworkflow.net>
2018-08-08 18:32:47 -0500

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 
-import 'src/assets/stylesheets/base.scss';
 
 
 function App({ name }) {
   return (
-    <Button variant="contained" color="primary" > Build Width, {name}!</Button>
+    <div> Build Width, {name}!</div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import axios from 'axios';
 import { render } from 'react-dom';
-import Grid from './components/Grid';
-import Tooltip from './components/Tooltip';
+
+var simple ="texto";
+
+import Service from './components/Service';
+import App from './components/App';
 
 
-const rootElement = document.querySelector('#root');
-if (rootElement) {
-  render(<div><Tooltip /><Grid /></div>, rootElement);
+
+var DOMParser = require('xmldom').DOMParser;
+
+class Hello extends React.Component {
+
+render() {
+this.text='';
+this.text= this.getXML();
+return <div>MasterCombo {this.props.toWhat}..{this.text}</div>;
 }
+
+getXML(){
+
+var http = require('http');
+var url = '/layout',
+data=this.getContent(url);
+data.then(function(html){
+
+  console.log(html);
+
+});
+
+return "help";
+}
+
+
+getContent(url){
+
+return new Promise((resolve, reject) => {
+    // select http or https module, depending on reqested url
+    const lib = url.startsWith('https') ? require('https') : require('http');
+    const request = lib.get(url, (response) => {
+      // handle http errors
+      if (response.statusCode < 200 || response.statusCode > 299) {
+         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+       }
+      // temporary data holder
+      const body = [];
+      // on every content chunk, push it to the data array
+      response.on('data', (chunk) => body.push(chunk));
+      // we are done, resolve promise with those joined chunks
+      response.on('end', () => resolve(body.join('')));
+    });
+    // handle connection errors of the request
+    request.on('error', (err) => reject(err))
+    })
+
+
+}
+
+
+
+}
+
+var task = 1;
+
+var name ='Alex';
+
+render(<div><Hello toWhat='???' /><App name={name} /><Service name='master' attribute='combo' /></div>, document.getElementById('root'));
